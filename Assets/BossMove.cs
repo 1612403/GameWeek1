5a084395051548ea79cf2411ca7d577898a431cf
1612403 <trhoainam1998@gmail.com>
2019-09-25 06:09:08 +0700

    My first commit

---
+++ Assets/BossMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMove : MonoBehaviour
+{
+    Vector2 newPos;
+    public float speed = 0.05f;
+    public bool moveLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = transform.position;
+        moveLeft = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= 7.5)
+            moveLeft = true;
+        if (transform.position.x <= 4.0)
+            moveLeft = false;
+        if (moveLeft)
+        {
+            newPos.x -= speed;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else 
+        {
+            newPos.x += speed;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        transform.position = newPos;
+    }
+}
---
+++ Assets/HeroRunController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRunController : MonoBehaviour
+{
+    Vector2 newPos;
+
+    public float speed = 0.025f;
+    public float jump = 250.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Animator>().SetTrigger("Run");
+            newPos.x -= speed;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Animator>().SetTrigger("Run");
+            newPos.x += speed;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        transform.position = newPos;
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jump));
+        }*/
+    }
+}
